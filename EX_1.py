8c15d19194494c570a0296bf17a92c86d63a4ef6
Dmytro Pashkov <dmitriypashkovs@gmail.com>
2023-11-09 15:59:53 +0200

    Done

--- EX_1.py
+++ EX_1.py
@@ -5,10 +5,13 @@ arr = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
 numbers = []
 zeros = []
 res = []
-for i in arr:
-    if i != 0:
-        numbers.append(i)
+i = 0
+while i < len(arr):
+    if arr[i] != 0:
+        numbers.append(arr[i])
     else:
-        zeros.append(i)
+        zeros.append(arr[i])
+    i += 1
 res.extend(numbers)
 res.extend(zeros)
+print(res)
