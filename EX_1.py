eb9de0a8ba7354a47cbae6327e77f837625d7a13
Dmytro Pashkov <dmitriypashkovs@gmail.com>
2023-11-09 16:55:17 +0200

    Done

--- EX_1.py
+++ EX_1.py
@@ -2,16 +2,8 @@
 Ваше завдання — змінити список так, щоб усі нулі опинилися наприкінці списку.
 Порядок ненульових чисел має зберегтися!'''
 arr = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-numbers = []
-zeros = []
-res = []
-i = 0
-while i < len(arr):
-    if arr[i] != 0:
-        numbers.append(arr[i])
-    else:
-        zeros.append(arr[i])
-    i += 1
-res.extend(numbers)
-res.extend(zeros)
-print(res)
+arr_clone = arr.copy()
+for i in arr:
+    if i == 0:
+        arr_clone.append(i)
+        arr_clone.remove(i)
