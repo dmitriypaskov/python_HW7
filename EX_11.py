2745b2b07df9b5878c1e8dd6063a8fe4489f54f6
Dmytro Pashkov <dmitriypashkovs@gmail.com>
2023-11-11 17:20:39 +0200

    Done

--- EX_11.py
+++ EX_11.py
@@ -1,12 +1,6 @@
 """Модифікувати калькулятор таким чином, щоб він працював доти, доки користувач цього хоче
 Тобто, потрібно робити запит до користувача на продовження роботи калькулятора після кожного обчислення
-- якщо користувач ввів yes (можна просто y), то нове обчислення, інакше - закінчення роботи.
-Наприклад:
-Enter first number: 1
-Enter second number: 2
-Enter action: +
-Your result is 3
-Do you want to perform another calculation? yes"""
+- якщо користувач ввів yes (можна просто y), то нове обчислення, інакше - закінчення роботи."""
 request_to_user = "y"
 while request_to_user == "yes" or request_to_user == "y":
     a = int(input('Please enter first number: '))
