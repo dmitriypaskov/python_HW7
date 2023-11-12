6388e0d7d744bb3a4fe4c32d515c6431793e296a
Dmytro Pashkov <dmitriypashkovs@gmail.com>
2023-11-12 14:08:42 +0200

    Done

--- EX_12.py
+++ EX_12.py
@@ -4,11 +4,7 @@ import string
 ніяких символів з набору string.punctuation не повинно бути, у тому числі й пробілів;
 підсумкова довжина hashtag має бути не більше 140 символів.
 кожне слово починається з великої літери.
-якщо довжина хештегу більше 140 символів - обрізати підсумковий рядок до 140 символів.
-Приклади:
-'Python Community' -> #PythonCommunity
-'i like python community!' -> #ILikePythonCommunity
-'Should, I. subscribe? Yes!' -> #ShouldISubscribeYes"""
+якщо довжина хештегу більше 140 символів - обрізати підсумковий рядок до 140 символів."""
 hashtag_billet = input("Please enter hashtag :\n")
 hashtag = ""
 max_len_hashtag = 140
