318c128615a330f6f03f02f4ae1bfa6ddc3347e8
Dmytro Pashkov <dmitriypashkovs@gmail.com>
2023-11-18 23:14:13 +0200

    Done

--- EX_18.py
+++ EX_18.py
@@ -10,10 +10,5 @@ import random
 def common_elements():
     matrix_3 = set([random.randrange(0, 100, 3) for j in range(random.randrange(3, 20))])
     matrix_5 = set([random.randrange(0, 100, 5) for j in range(random.randrange(3, 20))])
-    print(matrix_3)
-    print(matrix_5)
     common_set = matrix_3.intersection(matrix_5)
     return common_set
-
-
-print(common_elements())
